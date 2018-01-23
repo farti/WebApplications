17e1a45f3c11aac3c293dd293fb628e94cc3687f
Marcin Rakoczy <mrakoczy@wp.pl>
2018-01-23 11:49:12 +0100

    TCPClient

--- ConnectionTCPClient/Form1.cs
+++ ConnectionTCPClient/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Sockets;
 using System.Windows.Forms;
 
 namespace ConnectionTCPClient
@@ -15,6 +9,24 @@ namespace ConnectionTCPClient
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string host = textBox1.Text;
+            int port = System.Convert.ToInt16(numericUpDown1.Value);
+            try
+            {
+                TcpClient klient = new TcpClient(host, port);
+                listBox1.Items.Add("Nawiązano połączenie z " + host + " na porcie: " + port);
+                klient.Close();
+            }
+            catch(Exception ex)
+            {
+                listBox1.Items.Add("Błąd: Nie udało się nawiązać połączenia ! ");
+                MessageBox.Show(ex.ToString());
+            }
+
         }
     }
 }
