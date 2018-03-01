c4c03495a5e5a83ae529bd848afb564bd098ef0a
Marcin Rakoczy <mrakoczy@wp.pl>
2018-03-01 09:00:55 +0100

    Email

---
+++ Email/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Email
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxDo = new System.Windows.Forms.TextBox();
+            this.textBoxCc = new System.Windows.Forms.TextBox();
+            this.textBoxOd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBoxAdres = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxTemat = new System.Windows.Forms.TextBox();
+            this.textBoxList = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxDo);
+            this.groupBox1.Controls.Add(this.textBoxCc);
+            this.groupBox1.Controls.Add(this.textBoxOd);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(314, 112);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nagłówek";
+            // 
+            // textBoxDo
+            // 
+            this.textBoxDo.Location = new System.Drawing.Point(64, 44);
+            this.textBoxDo.Name = "textBoxDo";
+            this.textBoxDo.Size = new System.Drawing.Size(244, 20);
+            this.textBoxDo.TabIndex = 4;
+            // 
+            // textBoxCc
+            // 
+            this.textBoxCc.Location = new System.Drawing.Point(64, 68);
+            this.textBoxCc.Name = "textBoxCc";
+            this.textBoxCc.Size = new System.Drawing.Size(244, 20);
+            this.textBoxCc.TabIndex = 5;
+            // 
+            // textBoxOd
+            // 
+            this.textBoxOd.Location = new System.Drawing.Point(64, 20);
+            this.textBoxOd.Name = "textBoxOd";
+            this.textBoxOd.Size = new System.Drawing.Size(244, 20);
+            this.textBoxOd.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CC:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Do:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Od:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Controls.Add(this.textBoxAdres);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(334, 26);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(244, 112);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Serwer";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(9, 60);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(228, 43);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBoxAdres
+            // 
+            this.textBoxAdres.Location = new System.Drawing.Point(9, 33);
+            this.textBoxAdres.Name = "textBoxAdres";
+            this.textBoxAdres.Size = new System.Drawing.Size(228, 20);
+            this.textBoxAdres.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Adres:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Temat:";
+            // 
+            // textBoxTemat
+            // 
+            this.textBoxTemat.Location = new System.Drawing.Point(60, 145);
+            this.textBoxTemat.Name = "textBoxTemat";
+            this.textBoxTemat.Size = new System.Drawing.Size(511, 20);
+            this.textBoxTemat.TabIndex = 3;
+            // 
+            // textBoxList
+            // 
+            this.textBoxList.Location = new System.Drawing.Point(16, 171);
+            this.textBoxList.Multiline = true;
+            this.textBoxList.Name = "textBoxList";
+            this.textBoxList.Size = new System.Drawing.Size(555, 203);
+            this.textBoxList.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(252, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Wyślij";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 434);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxList);
+            this.Controls.Add(this.textBoxTemat);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxDo;
+        private System.Windows.Forms.TextBox textBoxCc;
+        private System.Windows.Forms.TextBox textBoxOd;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBoxAdres;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxTemat;
+        private System.Windows.Forms.TextBox textBoxList;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Email/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace Email
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MailAddress Od;
+            MailAddress Do;
+            MailAddress Cc = null;
+            SmtpClient klient = null;
+            try
+            {
+                Od = new MailAddress(textBoxOd.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Nieprawidłowy adres e-mail nadawcy");
+                textBoxOd.Text = String.Empty;
+                return;
+            }
+            try
+            {
+                Do = new MailAddress(textBoxDo.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Nieprawidłowy adres e-mail odbiorcy");
+                textBoxDo.Text = String.Empty;
+                return;
+            }
+            if (textBoxCc.Text != string.Empty)
+            {
+                try
+                {
+                    Cc = new MailAddress(textBoxCc.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Nieprawidłowy adres e-mail odbiorców");
+                    textBoxCc.Text = String.Empty;
+                    return;
+                }
+            }
+            MailMessage wiadomosc = new MailMessage(Od, Do);
+            wiadomosc.Subject = textBoxTemat.Text;
+            wiadomosc.Body = textBoxList.Text;
+            if (Cc != null)
+                wiadomosc.CC.Add(Cc);
+            try
+            {
+                klient = new SmtpClient(textBoxAdres.Text);
+                klient.Credentials = CredentialCache.DefaultNetworkCredentials;
+                klient.Send(wiadomosc);
+                listBox1.Items.Add("Wiadomość zostala wysłana");
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add("Brak połaczenia z serwerem : " + ex.Message);
+            }
+
+        }
+    }
+}
